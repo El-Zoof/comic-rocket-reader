948cc3d92100a565a15fe4da81cbd428e9cc4712
El-Zoof <el.zoof@gmail.com>
2026-03-31 11:38:56 +1100

    Refactor login check to use inline request config

--- chrome/extension/background.js
+++ chrome/extension/background.js
@@ -9,36 +9,45 @@ const defaultStorage = {
   backlog: []
 }
 
-// MV3: service workers don't inherit browser cookies automatically,
-// so we must send credentials explicitly with every request
-const requestConfig = { withCredentials: true }
-
 const checkLogin = () => {
-  http.get(LOGIN_CHECK, requestConfig).then(() => {
+  http.get(LOGIN_CHECK, { withCredentials: true }).then(() => {
     chrome.storage.local.get('comicRocketReader', storage => {
       const comicRocketReader = storage.comicRocketReader || defaultStorage
       comicRocketReader.isLoggedIn = true
+      comicRocketReader.lastError = null
       updateComics(comicRocketReader)
     })
-  }).catch(() => {
+  }).catch((err) => {
+    const errorDetail = err && err.response
+      ? `HTTP ${err.response.status}: ${err.response.statusText}`
+      : (err ? err.message : 'Unknown error')
     chrome.action.setBadgeText({ text: '' })
     chrome.storage.local.set({comicRocketReader: {
-      isLoggedIn: false
+      isLoggedIn: false,
+      lastError: errorDetail
     }})
   })
 }
 
 const updateComics = comicRocketReader => {
-  http.get(FETCH_COMICS, requestConfig).then((result) => {
+  http.get(FETCH_COMICS, { withCredentials: true }).then((result) => {
     const unreadPages = countUnreadPages(result.data.filter(comic => comicRocketReader.backlog.indexOf(comic.slug) === -1))
     chrome.action.setBadgeText({ text: unreadPages < 10000 ? '' + unreadPages : 'Lots' })
     comicRocketReader.comics = result.data
     chrome.storage.local.set({comicRocketReader})
+  }).catch((err) => {
+    const errorDetail = err && err.response
+      ? `HTTP ${err.response.status}: ${err.response.statusText}`
+      : (err ? err.message : 'Unknown error')
+    chrome.storage.local.set({comicRocketReader: {
+      ...comicRocketReader,
+      lastError: 'updateComics failed: ' + errorDetail
+    }})
   })
 }
 
 // MV3: Use chrome.alarms instead of setTimeout for periodic checks in service workers
-chrome.alarms.create('loginCheck', { periodInMinutes: 0.5 }) // every 30 seconds
+chrome.alarms.create('loginCheck', { periodInMinutes: 0.5 })
 
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === 'loginCheck') {
